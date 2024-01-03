8babfe0b90eceb1fddeb2d16f358c4fd5adb3ca7
Rahmatullah <mrahmatullah329@gmail.com>
2024-01-03 04:41:56 -0800

    Python program

---
+++ hello.py
@@ -0,0 +1,4 @@
+print("\nHello world!\n")
+a=int(input("enter the first number"))
+b=int(input("Enter second number"))
+print(a+b)
---
+++ rectangle.py
@@ -0,0 +1,7 @@
+l=float(input("Enter the length:"))
+b=float(input("Enter the breadth :"))
+area=l*b;
+print("area of rectangle :",area)
+peremeter=2*(l+b);
+print("peremeter of rectangle :",peremeter)
+
